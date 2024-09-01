1ec231524e1eb0fb4c2d9bb64bcb1abe56da423e
vijaydevops2024 <vijaydevops2024@gmail.com>
2024-09-01 09:15:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>My first PHP page version my Webhooks </h1>
 
 <?php
-echo "hello World from test FROM S2I 2nd time VERSION 3.2  LEARNING OPENSHIFT 4.14"
+echo "hello World from test FROM S2I 2nd time VERSION 3.2  LEARNING OPENSHIFT 4.16"
 ?>
 
 </body>
