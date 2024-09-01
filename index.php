5111909b865776fcc99285f93a854d68b6a52ee7
vijaydevops2024 <vijaydevops2024@gmail.com>
2024-09-01 09:08:57 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page version my Webhooks </h1>
+
+<?php
+echo "hello World from test FROM S2I 2nd time VERSION 3.2  LEARNING OPENSHIFT 4.14"
+?>
+
+</body>
+</html> 
